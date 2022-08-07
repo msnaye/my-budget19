94173e31a8ffad7c2db00b309fdaf31ad58d7016
msnaye <99111936+msnaye@users.noreply.github.com>
2022-08-07 11:08:26 -0500

    working on idb.js file

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -10,3 +10,30 @@ request.onupgradeneeded = function(event) {
     // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_transaction', { autoIncrement: true });
   };
+  // upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+      // if online then upload new transaction
+      uploadTransaction();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
+  // This function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object store for `new_pizza`
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    transactionObjectStore.add(record);
+  }
